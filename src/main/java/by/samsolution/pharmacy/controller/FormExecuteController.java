f136b56f301fd6fe4850160a19d77b16187117c2
Max <stelmachenokm@gmail.com>
2017-11-03 14:03:41 +0300

    Add table and form on UI

---
+++ src/main/java/by/samsolution/pharmacy/controller/FormExecuteController.java
@@ -0,0 +1,45 @@
+package by.samsolution.pharmacy.controller;
+
+import by.samsolution.pharmacy.exception.EntityAlreadyExistEsception;
+import by.samsolution.pharmacy.exception.ObjectValidationFailedException;
+import by.samsolution.pharmacy.service.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Enumeration;
+
+/**
+ * Created by y50-70 on 03.11.2017.
+ */
+public class FormExecuteController implements org.springframework.web.servlet.mvc.Controller {
+
+    private final Logger logger = LoggerFactory.getLogger(MyController.class);
+
+    private Service service;
+
+    public void setService(Service service) {
+        this.service = service;
+    }
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        ModelAndView mvc = new ModelAndView("medicaments");
+        Enumeration en = httpServletRequest.getParameterNames();
+        String stringMedicament = "";
+        while (en.hasMoreElements()) {
+            String name = (String) en.nextElement();
+            stringMedicament += " " + httpServletRequest.getParameter(name);
+        }
+
+        try {
+            service.addMedicament(stringMedicament);
+        } catch (EntityAlreadyExistEsception | NumberFormatException | ObjectValidationFailedException e) {
+            mvc.getModel().put("exceptionText", e.getMessage());
+        }
+        mvc.getModel().put("medicaments", service.getAllMedicaments());
+        return mvc;
+    }
+}
